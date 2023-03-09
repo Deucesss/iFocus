c14fe28b65e299b33e8ac42928a1dd277e45c9b8
Rencai Hu <rencaihu.dev@gmail.com>
2023-03-10 05:46:21 +1100

    experiment with gradle

--- feature/statistics/src/main/java/com/rencaihu/statistics/StatsFragment.kt
+++ feature/statistics/src/main/java/com/rencaihu/statistics/StatsFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.navigation.NavDeepLinkBuilder
+import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.findNavController
 import com.rencaihu.common.BaseFragment
 import com.rencaihu.statistics.databinding.FragmentStatsBinding
@@ -21,7 +24,12 @@ class StatsFragment: BaseFragment<FragmentStatsBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.root.setOnClickListener {
+            val req =
+                NavDeepLinkRequest.Builder
+                    .fromUri("android-app://example.google.app/home_fragment".toUri())
+                    .build()
+            findNavController().navigate(req)
+        }
     }
-
 }
